8155d0d91e4468b54011df7f8d22df01f4841d48
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2021-02-23 13:41:03 -0300

    moveIt

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/Profile.module.css'
+
+export default function Profile(){
+    
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/LuisHenriqueDaSilv.png" alt="Foto de perfil"/>
+            <div>
+                <strong>Luis Silva</strong>
+                <p>
+                    <img src="icons/level.svg" alt=""/>
+                    level 1
+                </p>
+            </div>
+        </div>
+    )
+}
